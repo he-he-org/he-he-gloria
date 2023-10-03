e6358ca79554bdd9127226ec2d6c8e250c8f08f2
Nikolai Mavrenkov <koluch@koluch.ru>
2023-10-03 20:37:26 +0200

    Add useIsMobile helper

--- app/helpers/browser.ts
+++ app/helpers/browser.ts
@@ -2,8 +2,24 @@
 import { useLayoutEffect, useState } from "react";
 
 function useWindow(): Window | null {
-  return global?.window ?? null
+  return global?.window ?? null;
 }
+
+export function useIsMobile() {
+  const window = useWindow();
+  const [value, setValue] = useState(false);
+  useLayoutEffect(() => {
+    const handler = () => {
+      setValue((window?.innerWidth ?? 0) < 1280);
+    };
+    window?.addEventListener(`resize`, handler);
+    return () => {
+      window?.removeEventListener("resize", handler);
+    };
+  }, []);
+  return value;
+}
+
 export function useScrollPosition() {
   const window = useWindow();
   const [position, setPosition] = useState(window?.scrollY ?? 0);
